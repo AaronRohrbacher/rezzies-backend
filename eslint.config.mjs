972d4c7fc6c67919ee4008b87ee9df8df465e68a
Aaron Rohrbacher <arjazz@icloud.com>
2024-09-27 03:22:47 -0700

    adds and fixes linters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,16 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: globals.browser,
+    },
+    ...pluginJs.configs.recommended,
+    rules: {
+      "no-unused-vars": "warn",  // Warn about unused variables
+    },
+  },
 ];
